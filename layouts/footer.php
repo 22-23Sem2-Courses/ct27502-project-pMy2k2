4a1f2080584e1657dd81544a1d01b65b007d148b
pMy2k2 <myb2014585@student.ctu.edu.vn>
2023-04-16 22:46:12 +0700

    layouts

---
+++ layouts/footer.php
@@ -0,0 +1,76 @@
+</div>
+
+<div class="container-pluid footer">
+    <section id="footer-button">
+        <div class="container-pluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 no-padding-r">
+                        <div class="footer-custom foooter-contact">
+                            <h3 class="tittle-footer"> Liên hệ</h3>
+                            <ul>
+                                <li>
+
+                                    <p><i class="fa fa-home" style="font-size: 16px;padding-right: 5px;"></i> An Bình - Ninh Kiều -CT </p>
+                                    <p><i class="sp-ic fa fa-mobile" style="font-size: 22px;padding-right: 5px;"></i> 0999999999</p>
+                                    <p><i class="sp-ic fa fa-envelope" style="font-size: 13px;padding-right: 5px;"></i> myb2014585@student.ctu.edu.vn</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 no-padding-x footer-width">
+                        <div class="footer-custom">
+                            <h3 class="tittle-footer"> Fanpage</h3>
+                            <div class="fanpage-fb">
+                                 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 no-padding-x" >
+                        <h3 class="tittle-footer">Bản đồ</h3>
+                        <div class="google-map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3303.746467397929!2d105.76743131373033!3d10.030157407112421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0883cc7d53ab5%3A0x329aed4526fa14de!2zSOG7mWkgdHLGsOG7nW5nIFLDuWEgxJBIQ1Q!5e0!3m2!1svi!2s!4v1681567728738!5m2!1svi!2s" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                     
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="ft-bottom">
+    </section>
+ 
+         
+
+         
+        <div class="scroll-top">
+            <a href="javascript:" id="return-to-top"><i class="fa fa-arrow-up"></i></a>
+            <script type="text/javascript">
+                // ===== Scroll to Top ====
+                $(window).scroll(function() {
+                    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+                        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+                    } else {
+                        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+                    }
+                });
+                $('#return-to-top').click(function() {      // When arrow is clicked
+                    $('body,html').animate({
+                        scrollTop : 0                       // Scroll to top of body
+                    }, 500);
+                });
+            </script>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script  src="<?php echo base_url() ?>public/frontend/js/slick.min.js"></script>
+
+</body>
+
+</html>
+
+ 
